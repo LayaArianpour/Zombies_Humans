3bb30bad3b7f1be258a61f8ae08d27e3a4ea26e5
laya <l.aryanpour80@gmail.com>
2020-10-30 22:27:13 +0330

    Part3: move boat

--- app/src/main/java/com/example/zombies_humans/MainActivity.java
+++ app/src/main/java/com/example/zombies_humans/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.zombies_humans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
@@ -21,11 +23,13 @@ public class MainActivity extends AppCompatActivity {
     HashMap<Object,String> existenceOfBoatPassengers=new HashMap<>();
     HashMap<String,Boolean> typeOfBoatPassengers=new HashMap<>();
     HashMap<Object,Boolean> passengers_IsBoarding=new HashMap<>();
-    ImageView[] listOfPasspassengers=new ImageView[2];
     int countOfHumansInLeft=3;
     int countOfHumansInRight=0;
     int countOfZombiesInLeft=3;
     int countOfZombiesInRight=0;
+    int countOfPassengers=0;
+    private ImageView passenger1,passenger2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
         container_boatLeft=findViewById(R.id.container_boatLeft);
         container_boatRight=findViewById(R.id.container_boatRight);
+        container_gameOver=findViewById(R.id.container_gameOver);
+        container_gameWin=findViewById(R.id.container_gameWin);
+
+        btn_go=findViewById(R.id.btn_go);
+        btn_playAgain_gameOver=findViewById(R.id.btn_playAgain_Gameover);
+        btn_playAgain_win=findViewById(R.id.btn_playAgain_win);
 
         passengers_IsBoarding.put(img_human1Left,false);
         passengers_IsBoarding.put(img_human2Left,false);
@@ -77,5 +87,367 @@ public class MainActivity extends AppCompatActivity {
 
         typeOfBoatPassengers.put("passengers1_isHuman",false);
         typeOfBoatPassengers.put("passengers2_isHuman",false);
+
+        img_human1Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human1Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human1Left,img_human1Right,isHuman,"human1",true);
+                }
+            }
+        });
+
+        img_human2Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human2Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human2Left,img_human2Right,isHuman,"human2",true);
+                }
+            }
+        });
+
+        img_human3Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human3Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human3Left,img_human3Right,isHuman,"human3",true);
+                }
+            }
+        });
+
+        img_human1Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human1Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human1Right,img_human1Left,isHuman,"human1",false);
+                }
+            }
+        });
+
+        img_human2Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human2Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human2Right,img_human2Left,isHuman,"human2",false);
+                }
+            }
+        });
+
+        img_human3Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = true;
+                if (!passengers_IsBoarding.get(img_human3Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_human3Right,img_human3Left,isHuman,"human3",false);
+                }
+            }
+        });
+
+
+        img_zombie1Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie1Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie1Left,img_zombie1Right,isHuman,"zombie1",true);
+                }
+            }
+        });
+
+        img_zombie2Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie2Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie2Left,img_zombie2Right,isHuman,"zombie2",true);
+                }
+            }
+        });
+
+        img_zombie3Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie3Left)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie3Left,img_zombie3Right,isHuman,"zombie3",true);
+                }
+            }
+        });
+
+        img_zombie1Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie1Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie1Right,img_zombie1Left,isHuman,"zombie1",false);
+                }
+            }
+        });
+
+        img_zombie2Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie2Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie2Right,img_zombie2Left,isHuman,"zombie2",false);
+                }
+            }
+        });
+
+        img_zombie3Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHuman = false;
+                if (!passengers_IsBoarding.get(img_zombie3Right)) { //if human not in boat
+                    checkBeforeGettingOnBoat(img_zombie3Right,img_zombie3Left,isHuman,"zombie3",false);
+                }
+            }
+        });
+
+        img_passenger1Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOffBoat(img_passenger1Left, boatInLeft);
+            }
+        });
+
+        img_passenger2Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOffBoat(img_passenger2Left, boatInLeft);
+            }
+        });
+
+        img_passenger1Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOffBoat(img_passenger1Right, boatInLeft);
+            }
+        });
+
+        img_passenger2Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getOffBoat(img_passenger2Right, boatInLeft);
+            }
+        });
+
+        btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveBoat(boatInLeft);
+            }
+        });
     }
+
+    private void moveBoat(boolean isBoatInLeft) {
+        if(countOfPassengers>0){
+            if(isBoatInLeft){
+                if(passenger1!=null){
+                    img_passenger1Right.setVisibility(View.VISIBLE);
+                    img_passenger1Right.setImageDrawable(passenger1.getDrawable());
+                }
+                if(passenger2!=null){
+                    img_passenger2Right.setVisibility(View.VISIBLE);
+                    img_passenger2Right.setImageDrawable(passenger2.getDrawable());
+                }
+                container_boatLeft.setVisibility(View.INVISIBLE);
+                container_boatRight.setVisibility(View.VISIBLE);
+                boatInLeft=false;
+            }else{
+                if(passenger1!=null){
+                    img_passenger1Left.setVisibility(View.VISIBLE);
+                    img_passenger1Left.setImageDrawable(passenger1.getDrawable());
+                }
+                if(passenger2!=null){
+                    img_passenger2Left.setVisibility(View.VISIBLE);
+                    img_passenger2Left.setImageDrawable(passenger2.getDrawable());
+                }
+                container_boatRight.setVisibility(View.INVISIBLE);
+                container_boatLeft.setVisibility(View.VISIBLE);
+                boatInLeft=true;
+            }
+        }else{
+            Toast.makeText(MainActivity.this,"Boat is Empty!!\nplease getting on boat passenger",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getOffBoat(ImageView imgPassenger, boolean boatInLeft) {
+        String nameOfPassengr=existenceOfBoatPassengers.get(imgPassenger);
+        if(boatInLeft) {
+            switch (nameOfPassengr) {
+                case "zombie1":
+                    img_zombie1Left.setVisibility(View.VISIBLE);
+                    img_zombie1Right.setVisibility(View.INVISIBLE);
+                    break;
+                case "zombie2":
+                    img_zombie2Left.setVisibility(View.VISIBLE);
+                    img_zombie2Right.setVisibility(View.INVISIBLE);
+                    break;
+                case "zombie3":
+                    img_zombie3Left.setVisibility(View.VISIBLE);
+                    img_zombie3Right.setVisibility(View.INVISIBLE);
+                    break;
+                case "human1":
+                    img_human1Left.setVisibility(View.VISIBLE);
+                    img_human1Right.setVisibility(View.INVISIBLE);
+                    break;
+                case "human2":
+                    img_human2Left.setVisibility(View.VISIBLE);
+                    img_human2Right.setVisibility(View.INVISIBLE);
+                    break;
+                case "human3":
+                    img_human3Left.setVisibility(View.VISIBLE);
+                    img_human3Right.setVisibility(View.INVISIBLE);
+                    break;
+            }
+        }else{
+            switch (nameOfPassengr) {
+                case "zombie1":
+                    img_zombie1Left.setVisibility(View.INVISIBLE);
+                    img_zombie1Right.setVisibility(View.VISIBLE);
+                    break;
+                case "zombie2":
+                    img_zombie2Left.setVisibility(View.INVISIBLE);
+                    img_zombie2Right.setVisibility(View.VISIBLE);
+                    break;
+                case "zombie3":
+                    img_zombie3Left.setVisibility(View.INVISIBLE);
+                    img_zombie3Right.setVisibility(View.VISIBLE);
+                    break;
+                case "human1":
+                    img_human1Left.setVisibility(View.INVISIBLE);
+                    img_human1Right.setVisibility(View.VISIBLE);
+                    break;
+                case "human2":
+                    img_human2Left.setVisibility(View.INVISIBLE);
+                    img_human2Right.setVisibility(View.VISIBLE);
+                    break;
+                case "human3":
+                    img_human3Left.setVisibility(View.INVISIBLE);
+                    img_human3Right.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+        if(imgPassenger==img_passenger1Left || imgPassenger==img_passenger1Right){
+            passenger1=null;
+        }
+        if(imgPassenger==img_passenger2Left || imgPassenger==img_passenger2Right){
+            passenger2=null;
+        }
+        countOfPassengers--;
+        imgPassenger.setVisibility(View.INVISIBLE);
+        existenceOfBoatPassengers.remove(imgPassenger);
+        existenceOfBoatPassengers.put(imgPassenger,null);
+    }
+
+    private void checkBeforeGettingOnBoat(ImageView imgInBoat, ImageView imgAdverse, boolean isHuman,String nameOfPassenger,boolean isLeftGettinOn) {
+
+        if(isLeftGettinOn){
+            if(boatInLeft){
+                gettingOnBoat(imgInBoat,imgAdverse,isHuman,true,nameOfPassenger);
+            }else {
+                Toast.makeText(MainActivity.this, "Opsss boat in right!!\n can not move!!", Toast.LENGTH_SHORT).show();
+            }
+        }else if(!isLeftGettinOn){
+            if(!boatInLeft){
+                gettingOnBoat(imgInBoat,imgAdverse,isHuman,false,nameOfPassenger);
+            }else {
+                Toast.makeText(MainActivity.this, "Opsss boat in left!!\n can not move!!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void gettingOnBoat(ImageView imgInBoat, ImageView imgAdverse, boolean isHuman, boolean toRightBoat,String nameOfPassenger) {
+
+        if(toRightBoat){ //boat in left
+            if(existenceOfBoatPassengers.get(img_passenger1Left)==null) { //if boat Passengers1 is empty
+                imgInBoat.setVisibility(View.INVISIBLE);
+                imgAdverse.setVisibility(View.INVISIBLE);
+                img_passenger1Left.setImageDrawable(imgInBoat.getDrawable());
+                img_passenger1Left.setVisibility(View.VISIBLE);
+                existenceOfBoatPassengers.remove(img_passenger1Left);
+                existenceOfBoatPassengers.put(img_passenger1Left,nameOfPassenger);
+                passenger1=imgInBoat;
+                countOfPassengers++;
+                /*passengers_IsBoarding.remove(imgInBoat);
+                passengers_IsBoarding.put(imgInBoat,true);
+                passengers_IsBoarding.remove(imgAdverse);
+                passengers_IsBoarding.put(imgAdverse,true);
+                typeOfBoatPassengers.remove("passengers1_isHuman");
+                typeOfBoatPassengers.put("passengers1_isHuman",isHuman);*/
+                //callListOfPasspassengers(content,true);
+                //computingCountOfItems(isHuman,true,true);
+            }
+            else if(existenceOfBoatPassengers.get(img_passenger2Left)==null){//if boat Passengers1 is empty
+                imgInBoat.setVisibility(View.INVISIBLE);
+                imgAdverse.setVisibility(View.INVISIBLE);
+                img_passenger2Left.setImageDrawable(imgInBoat.getDrawable());
+                img_passenger2Left.setVisibility(View.VISIBLE);
+                existenceOfBoatPassengers.remove(img_passenger2Left);
+                existenceOfBoatPassengers.put(img_passenger2Left,nameOfPassenger);
+                passenger2=imgInBoat;
+                countOfPassengers++;
+               /* passengers_IsBoarding.remove(imgInBoat);
+                passengers_IsBoarding.put(imgInBoat,true);
+                passengers_IsBoarding.remove(imgAdverse);
+                passengers_IsBoarding.put(imgAdverse,true);
+                typeOfBoatPassengers.remove("passengers2_isHuman");
+                typeOfBoatPassengers.put("passengers2_isHuman",isHuman);*/
+                //callListOfPasspassengers(content,true);
+                //computingCountOfItems(isHuman,true,true);
+            }
+            else{
+                Toast.makeText(MainActivity.this,"boat is full!!!!",Toast.LENGTH_SHORT).show();
+            }
+        }else{// boat in right
+            if(existenceOfBoatPassengers.get(img_passenger1Right)==null) { //if boat Passengers1 is empty
+                imgInBoat.setVisibility(View.INVISIBLE);
+                imgAdverse.setVisibility(View.INVISIBLE);
+                img_passenger1Right.setImageDrawable(imgInBoat.getDrawable());
+                img_passenger1Right.setVisibility(View.VISIBLE);
+                existenceOfBoatPassengers.remove(img_passenger1Right);
+                existenceOfBoatPassengers.put(img_passenger1Right,nameOfPassenger);
+                passenger1=imgInBoat;
+                countOfPassengers++;
+               /* passengers_IsBoarding.remove(imgInBoat);
+                passengers_IsBoarding.put(imgInBoat,true);
+                passengers_IsBoarding.remove(imgAdverse);
+                passengers_IsBoarding.put(imgAdverse,true);
+                typeOfBoatPassengers.remove("passengers1_isHuman");
+                typeOfBoatPassengers.put("passengers1_isHuman",isHuman);*/
+                //callListOfPasspassengers(content,true);
+                //computingCountOfItems(isHuman,true,true);
+            }
+            else if(existenceOfBoatPassengers.get(img_passenger2Right)==null){ //if boat Passengers2 is empty
+                imgInBoat.setVisibility(View.INVISIBLE);
+                imgAdverse.setVisibility(View.INVISIBLE);
+                img_passenger2Right.setImageDrawable(imgInBoat.getDrawable());
+                img_passenger2Right.setVisibility(View.VISIBLE);
+                existenceOfBoatPassengers.remove(img_passenger2Right);
+                existenceOfBoatPassengers.put(img_passenger2Right,nameOfPassenger);
+                passenger2=imgInBoat;
+                countOfPassengers++;
+                /*passengers_IsBoarding.remove(imgInBoat);
+                passengers_IsBoarding.put(imgInBoat,true);
+                passengers_IsBoarding.remove(imgAdverse);
+                passengers_IsBoarding.put(imgAdverse,true);
+                typeOfBoatPassengers.remove("passengers2_isHuman");
+                typeOfBoatPassengers.put("passengers2_isHuman",isHuman);*/
+                //callListOfPasspassengers(content,true);
+                //computingCountOfItems(isHuman,true,true);
+            }
+            else{
+                Toast.makeText(MainActivity.this,"boat is full!!!!",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
 }
